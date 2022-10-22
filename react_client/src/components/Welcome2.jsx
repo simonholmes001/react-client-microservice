5e03599eff3f72566a4b8a306b61825c38f396b8
simonholmes001 <simonholmesabc@gmail.com>
2022-10-22 07:14:35 +0200

    Update UI to display two images

--- react_client/src/components/Welcome2.jsx
+++ react_client/src/components/Welcome2.jsx
@@ -7,54 +7,88 @@ const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-xl font-light text-white";
 
 const Welcome2 = () => {
-  const [filename, setFilename] = useState("");
-  const [result, setResult] = useState("");
+  const [leftfilename, setLeftfilename] = useState("");
+  const [leftresult, setLeftresult] = useState("");
+
+  const [rightfilename, setRightfilename] = useState("");
+  const [rightresult, setRightresult] = useState("");
 
   useEffect(() => {
     socket.on("sendingMessage", (arg) => {
       const data = JSON.stringify(arg);
       const data2 = JSON.parse(data);
-      console.log(`RESULT : ${data2.result}`);
-      console.log(`FILENAME : ${data2.filename}`);
-      setFilename(data2.filename);
-      setResult(data2.result);
+      console.log(`RESULT_1 : ${data2.result1}`);
+      console.log(`FILENAME_1 : ${data2.filename1}`);
+      setLeftfilename(data2.filename1);
+      setLeftresult(data2.result1);
+      setRightfilename(data2.filename2);
+      setRightresult(data2.result2);
     });
   }, []);
 
-  const imageUrl = `http://localhost:8000/getImage?input=${filename}`;
+ 
+  const leftImageUrl = `http://localhost:8000/getImage?input=${leftfilename}`;
+  const rightImageUrl = `http://localhost:8000/getImage?input=${rightfilename}`;
 
-  const [img, setImg] = useState();
+  const [leftImg, setLeftimg] = useState();
+  const [rightImg, setRightimg] = useState();
 
   useEffect(() => {
     const fetchImage = async () => {
-      const res = await fetch(imageUrl);
+      const res = await fetch(leftImageUrl);
       const imageBlob = await res.blob();
       const imageObjectURL = URL.createObjectURL(imageBlob);
-      setImg(imageObjectURL);
+      setLeftimg(imageObjectURL);
     };
     fetchImage();
-  }, [imageUrl]);
+  }, [leftImageUrl]);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const res = await fetch(rightImageUrl);
+      const imageBlob = await res.blob();
+      const imageObjectURL = URL.createObjectURL(imageBlob);
+      setRightimg(imageObjectURL);
+    };
+    fetchImage();
+  }, [rightImageUrl]);
 
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex md:flex-rox flex-col justify-between md:p-20">
         <div className="flex flex-col justy-start md:mr-10 md:mt-10 lg:-mt-5">
-          <div className="justify-between flex flex-col text-left text-2xl lg:-ml-20 md:ml-0 mb-5 tracking-[0.1rem] text-white font-light md:w-9/12 w-11/12">
-            <h1 className="font-semibold p-2 text-left">
-              Filename: &nbsp; {filename}
-            </h1>
-            <h1 className="font-semibold p-2 text-left">
-              Result: &nbsp; {result}
-            </h1>
+          <div className="w-4/5 ml-40 grid grid-cols-2">
+            <div className="grid grid-rows-2 justify-between text-left text-2xl tracking-[0.1rem] text-white font-light">
+              <h1 className="font-semibold p-2 text-left">
+                Filename: &nbsp; {leftfilename}
+              </h1>
+              <h1 className="font-semibold p-2 text-left">
+                Result: &nbsp; {leftresult}
+              </h1>
+              <img
+                src={leftImg}
+                alt="pic here"
+                className="text-white flex flex-1 justify-center items-center ml-10 mr-40 mb-40 mt-5"
+                width="400"
+                height="400"
+              />
+            </div>
+            <div className="grid grid-rows-2 justify-between text-left text-2xl tracking-[0.1rem] text-white font-light">
+              <h1 className="font-semibold p-2 text-left">
+                Filename: &nbsp; {rightfilename}
+              </h1>
+              <h1 className="font-semibold p-2 text-left">
+                Result: &nbsp; {rightresult}
+              </h1>
+              <img
+                src={rightImg}
+                alt="pic here"
+                className="text-white flex flex-1 justify-center items-center ml-10 mr-40 mb-40 mt-5"
+                width="400"
+                height="400"
+              />
+            </div>
           </div>
-
-          <img
-            src={img}
-            alt="pic here"
-            className="text-white flex flex-1 justify-center items-center ml-40 mr-40 mb-40 mt-5"
-            width="600"
-            height="600"
-          />
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-15">
             <div className={`rounded-tl-2xl ${commonStyles}`}>Powered</div>
